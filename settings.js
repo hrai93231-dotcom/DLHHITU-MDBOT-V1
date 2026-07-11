98776865aa6911e63bbe0dd212d8128f17b8db99
hrai93231-dotcom <hrai93231@gmail.com>
2026-07-11 10:35:56 +0500

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,6 @@
+module.exports = {
+    giphyApiKey: process.env.GIPHY_API_KEY || 'dc6zaTOxFJmzC',
+    ownerNumber: process.env.OWNER_NUMBER || '923368443729',
+    botName: '𝗗𝗟𝗛-𝗛𝗜𝗧𝗨 𝗠𝗗 𝗕𝗢𝗧',
+    ownerName: '𝖧𝖨𝖳𝖴'
+};
